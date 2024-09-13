723313fb29d3cb83f9ba489d80a74e1f8c9eff26
kostasafe <22775121+kostasafe@users.noreply.github.com>
2024-09-13 20:05:19 +0300

    Set up player character with its movement and borders

---
+++ main.py
@@ -0,0 +1,63 @@
+import turtle
+import os
+
+#seting up screen
+myscreen = turtle.Screen()
+myscreen.bgcolor("black")
+myscreen.title("Space Invaders")
+
+#Border drawing
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300,-300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+
+#Player Turtle
+player = turtle.Turtle()
+player.color("yellow")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0,-250)
+player.setheading(90)
+
+playerspeed = 15
+
+#Player Movement Left-Right
+def move_left():
+    x = player.xcor()
+    x -= playerspeed
+    if x <-280:
+        x = -280
+    player.setx(x)
+
+def move_right():
+    x = player.xcor()
+    x += playerspeed
+    if x > +280:
+        x= +280
+    player.setx(x)
+
+#Keyboard Bindings
+turtle.listen()
+turtle.onkey(move_left,"Left")
+turtle.onkey(move_right, "Right")
+
+
+
+
+
+
+
+
+
+
+delay =  input("Press enter to finish.")
